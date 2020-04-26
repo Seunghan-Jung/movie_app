d75a6a91fb6df61c85e1608662881babeb550092
ajtwlstmdgks <ajtwlstmdgks@naver.com>
2020-04-26 20:45:52 +0900

    Updates

---
+++ precache-manifest.ddd3b3ee04ffecabe49cfc91077e125e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6764af8e82374959c432ea30f9f411",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3f359d77eada957beaaf",
+    "url": "/movie_app/static/css/main.d9de3c2f.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f359d77eada957beaaf",
+    "url": "/movie_app/static/js/main.5483f6be.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
